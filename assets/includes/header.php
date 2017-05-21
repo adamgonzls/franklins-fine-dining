e4bd699b1ce277e5dc54ad29083ce17556d73b94
Adam Gonzales <adamgonzales1@gmail.com>
2017-05-21 13:00:46 -0700

    initial commit

---
+++ assets/includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="/assets/styles.css">
+</head>
+<body id="final-example">
+	<div class="wrapper">
+		<div class="banner">
+			<a href="/" title="Return to Home">
+				<img src="/assets/img/banner.png" alt="Franklin's Fine Dining">
+			</a>
+		</div><!-- banner -->
+		<div id="nav">
+			<?php include('/assets/includes/nav.php') ?>
+		</div><!-- nav -->
+		<div class="content">
